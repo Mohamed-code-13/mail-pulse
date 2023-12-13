e929fc4e10169d704d92ba107cff43363d58cd14
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-12-13 05:54:02 +0200

    Create a login form

--- front-end-mail-server/src/App.vue
+++ front-end-mail-server/src/App.vue
@@ -1,85 +1,28 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <LoginForm />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import LoginForm from './views/LoginForm.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+export default {
+  components: { LoginForm }
 }
+</script>
 
-nav {
-  width: 100%;
-  font-size: 12px;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+body {
+  margin: 0px;
+  padding: 0px;
+  background: #eee;
 }
 </style>
---
+++ front-end-mail-server/src/views/LoginForm.vue
@@ -0,0 +1,79 @@
+<template>
+  <form @submit.prevent>
+    <h2>Login</h2>
+
+    <label>Email:</label>
+    <input type="email" name="email" required />
+
+    <label>Password:</label>
+    <input type="password" name="password" required />
+
+    <button>Login</button>
+
+    <p>Don't have an account? <span>Sign up</span></p>
+  </form>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+h2 {
+  border-bottom: 1px solid #555;
+}
+
+form {
+  width: 420px;
+  left: 50%;
+  position: relative;
+  margin: 0 auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+
+input {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+
+button {
+  display: block;
+  margin-top: 15px;
+  padding: 15px;
+  width: 100%;
+  color: white;
+  background: green;
+  border: none;
+  border-radius: 12px;
+  font-weight: bold;
+  font-size: 16px;
+}
+
+p {
+  color: #555;
+  margin-top: 12px;
+  text-align: center;
+}
+
+span {
+  color: green;
+  cursor: pointer;
+}
+</style>
