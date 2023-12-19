69ce6fa7f5c34a5e42b4faf373046d759a0d7441
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-12-19 17:10:52 +0200

    Adding a title to the compose dialog and styling it.

--- front-end-mail-server/src/components/ComposeDialog.vue
+++ front-end-mail-server/src/components/ComposeDialog.vue
@@ -1,5 +1,6 @@
 <template>
   <dialog open>
+    <h2>New Email</h2>
     <form @submit.prevent>
       <label>From:</label>
       <input type="text" value="test@test.test" disabled />
@@ -49,6 +50,11 @@ dialog {
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
   border: 1px solid gray;
   border-radius: 12px;
+}
+
+h2 {
+  border-bottom: 1px solid gray;
+  margin-bottom: 6px;
 }
 
 form {
