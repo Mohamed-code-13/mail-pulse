98e1f4fd5365ec0590cbe19041b83b63703d76c9
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-12-20 13:45:52 +0200

    Making the ListOfEmails take emails dynamically

--- front-end-mail-server/src/components/ListEmails.vue
+++ front-end-mail-server/src/components/ListEmails.vue
@@ -1,47 +1,15 @@
 <template>
   <div id="list-emails"></div>
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
-  <Email :email="email" />
+
+  <Email v-for="email in emails" :email="email" />
 </template>
 
 <script>
-import { ref } from 'vue'
 import Email from '@/components/Email.vue'
 
 export default {
-  components: { Email },
-  setup() {
-    const email = ref({
-      id: 0,
-      sender: 'mohamed',
-      subject: 'Programming',
-      receiver: 'ahmed@test.com',
-      description:
-        'Props attributes are written with a dash - to separate words (kebab-case) in the <template> tag, but kebab-case is not legal in JavaScript. So instead we need to write the attribute names as camelCase in JavaScript, and Vue understands this automatically!',
-      date: '12/13/2023 12:16'
-    })
-
-    return { email }
-  }
+  props: ['emails'],
+  components: { Email }
 }
 </script>
 
--- front-end-mail-server/src/views/Inbox.vue
+++ front-end-mail-server/src/views/Inbox.vue
@@ -1,16 +1,54 @@
 <template>
   <div id="inbox">
     <SearchBar title="Search mail" />
-    <ListEmails />
+    <ListEmails :emails="filterEmails" />
   </div>
 </template>
 
 <script>
+import { computed, ref } from 'vue'
 import ListEmails from '@/components/ListEmails.vue'
 import SearchBar from '@/components/SearchBar.vue'
 
 export default {
-  components: { SearchBar, ListEmails }
+  components: { SearchBar, ListEmails },
+  setup() {
+    const searchValue = ref('')
+
+    const emails = ref([
+      {
+        id: 0,
+        sender: 'Ahmed',
+        receiver: 'mohamed@test.com',
+        subject: 'College',
+        description: 'Testing some subjects',
+        date: '12/13/2023 12:16'
+      },
+      {
+        id: 1,
+        sender: 'mohamed',
+        receiver: 'ahmed@test.com',
+        subject: 'Programming',
+        description:
+          'Props attributes are written with a dash - to separate words (kebab-case) in the <template> tag, but kebab-case is not legal in JavaScript. So instead we need to write the attribute names as camelCase in JavaScript, and Vue understands this automatically!',
+        date: '12/13/2023 12:16'
+      },
+      {
+        id: 2,
+        sender: 'Ali',
+        receiver: 'ahmed@test.com',
+        subject: 'Important',
+        description: "We didn't finish anything and the deadline is coming for us!!!",
+        date: '12/13/2023 12:16'
+      }
+    ])
+
+    const filterEmails = computed(() => {
+      return emails.value.filter((e) => e.description.includes(searchValue.value))
+    })
+
+    return { emails, filterEmails, searchValue }
+  }
 }
 </script>
 
