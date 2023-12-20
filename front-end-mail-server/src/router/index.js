24ca8ec86c245fc04d7a0e9f13423d9c4d2ce148
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-12-20 13:02:49 +0200

    Create and style the Folder view.

--- front-end-mail-server/src/router/index.js
+++ front-end-mail-server/src/router/index.js
@@ -4,6 +4,7 @@ import SignUpForm from '../views/SignUpForm.vue'
 import Home from '../views/Home.vue'
 import Inbox from '../views/Inbox.vue'
 import Contacts from '../views/Contacts.vue'
+import Folders from '../views/Folders.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,7 +52,7 @@ const router = createRouter({
         {
           path: 'folders',
           // name: 'inbox',
-          component: Inbox
+          component: Folders
         },
         {
           path: 'trash',
---
+++ front-end-mail-server/src/views/Folders.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="folders">
+    <div id="add-folder">
+      <span class="material-symbols-outlined"> folder </span>
+
+      <input type="text" placeholder="Add new folder" />
+
+      <button>+ Add folder</button>
+    </div>
+
+    <div v-for="item in folders" class="item">
+      <span class="material-symbols-outlined"> folder </span>
+
+      <h2 @click="goToFolder(item)">
+        {{ item }}
+      </h2>
+
+      <div id="btns">
+        <span class="material-symbols-outlined edit"> edit </span>
+
+        <span class="material-symbols-outlined delete"> delete </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const folders = ref(['College', 'Programming', 'Sports', 'Others'])
+    const goToFolder = (item) => {}
+    return { folders, goToFolder }
+  }
+}
+</script>
+
+<style scoped>
+#folders {
+  flex: 0.8;
+  overflow-x: hidden;
+  height: 90vh;
+  background-color: #eeeeeead;
+  border-radius: 12px;
+}
+
+#add-folder {
+  display: flex;
+  width: 400px;
+  align-items: center;
+  background-color: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 18px;
+}
+
+#add-folder span {
+  color: #777;
+  background-color: transparent;
+}
+
+#add-folder input {
+  border: none;
+  width: 60%;
+  padding: 10px;
+  outline: none;
+  font-size: medium;
+  background-color: transparent;
+}
+
+#add-folder button {
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  background-color: green;
+}
+
+.item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: gray;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 12px;
+}
+
+h2 {
+  color: #eee;
+  cursor: pointer;
+}
+
+.delete {
+  background-color: red;
+  color: white;
+  padding: 5px;
+  margin-left: 10px;
+  border-radius: 8px;
+}
+
+.edit {
+  background-color: white;
+  color: gray;
+  padding: 5px;
+  margin-left: 10px;
+  border-radius: 8px;
+}
+</style>
