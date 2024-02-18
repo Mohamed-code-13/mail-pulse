ce22a21fbd53f8b278cd2e242d33c62f514465dd
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-02-18 21:10:55 +0200

    Creating the spring boot project.

---
+++ back-end-mail-server/src/main/java/com/mohamedcode/mailpulse/MailPulseApplication.java
@@ -0,0 +1,13 @@
+package com.mohamedcode.mailpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailPulseApplication.class, args);
+	}
+
+}
