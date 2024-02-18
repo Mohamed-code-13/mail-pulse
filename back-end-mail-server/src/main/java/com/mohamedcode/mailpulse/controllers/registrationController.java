60b4e71379898c212e49960796b7606108676e84
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-02-19 00:06:14 +0200

    Created a registration controller and successfully creating new accounts and save them in the db.

---
+++ back-end-mail-server/src/main/java/com/mohamedcode/mailpulse/controllers/registrationController.java
@@ -0,0 +1,34 @@
+package com.mohamedcode.mailpulse.controllers;
+
+import com.mohamedcode.mailpulse.models.UserModel;
+import com.mohamedcode.mailpulse.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/register")
+public class registrationController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("signup")
+    public ResponseEntity<Map<String, String>> signUpUser(@RequestBody Map<String, Object> userMap) {
+        String name = (String) userMap.get("name");
+        String email = (String) userMap.get("email");
+        String password = (String) userMap.get("password");
+
+        UserModel user = userService.registerUser(name, email, password);
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "Registered successfully");
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+}
