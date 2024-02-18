02c447ad7d668287480b32d75721bb259864181a
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-02-18 22:52:06 +0200

    Creating UserModel, UserService, and CustomAuthException.

---
+++ back-end-mail-server/src/main/java/com/mohamedcode/mailpulse/exceptions/CustomAuthException.java
@@ -0,0 +1,8 @@
+package com.mohamedcode.mailpulse.exceptions;
+
+public class CustomAuthException extends RuntimeException {
+
+    public CustomAuthException(String message) {
+        super(message);
+    }
+}
