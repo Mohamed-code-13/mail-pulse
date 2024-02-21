6d1ec94bf6dfbb4c385169f757e57bd9a97b6b47
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-02-21 21:37:43 +0200

    Created EmailService and connected it with the EmailRepository.

---
+++ back-end-mail-server/src/main/java/com/mohamedcode/mailpulse/services/EmailServiceImpl.java
@@ -0,0 +1,32 @@
+package com.mohamedcode.mailpulse.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class EmailServiceImpl implements EmailService {
+    @Autowired
+    EmailService emailService;
+
+    @Override
+    public void deleteEmail(Integer emailID) {
+        // delete email from the current folder
+        // add the deleted email to the trash folder
+        // if the email in the trash folder delete it from the db
+        emailService.deleteEmail(emailID);
+    }
+
+    @Override
+    public void sendEmail(Integer senderId, List<String> receivers,
+                          String subject, String body, Integer priority,
+                          List<MultipartFile> files) {
+        // create a new email and add it to the db
+        emailService.sendEmail(senderId, receivers,
+                               subject, body, priority, files);
+    }
+}
