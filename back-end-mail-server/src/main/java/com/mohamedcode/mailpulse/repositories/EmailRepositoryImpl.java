05a584d402d202a8c1c82784f98ba8985ee754f1
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-02-25 18:50:11 +0200

    When deleting, moving the emails to the trash first.

--- back-end-mail-server/src/main/java/com/mohamedcode/mailpulse/repositories/EmailRepositoryImpl.java
+++ back-end-mail-server/src/main/java/com/mohamedcode/mailpulse/repositories/EmailRepositoryImpl.java
@@ -1,9 +1,7 @@
 package com.mohamedcode.mailpulse.repositories;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,6 +23,13 @@ public class EmailRepositoryImpl implements EmailRepository {
             	?, ?, ?, ?, ?, ?
             );
             """;
+    private static final String SQL_MOVE_EMAIL_TO_TRASH = """
+            UPDATE emails
+            SET deleted = TRUE,
+                deleted_at = CURRENT_TIMESTAMP
+            WHERE email_id = ?
+            """;
+    private static final String SQL_IS_EMAIL_IN_TRASH = "SELECT deleted FROM emails WHERE email_id = ?";
     private static final String SQL_DELETE_EMAIL_BY_ID = "DELETE FROM emails WHERE email_id = ?";
     private static final String SQL_GET_USER_ID_BY_EMAIL = "SELECT user_id FROM users WHERE email = ?";
 
@@ -33,7 +38,10 @@ public class EmailRepositoryImpl implements EmailRepository {
 
     @Override
     public void deleteEmail(Integer emailID) {
-        jdbcTemplate.update(SQL_DELETE_EMAIL_BY_ID, emailID);
+        if (isEmailInTrash(emailID))
+            deleteEmailFromDB(emailID);
+        else
+            moveEmailToTrash(emailID);
     }
 
     @Override
@@ -48,5 +56,17 @@ public class EmailRepositoryImpl implements EmailRepository {
 
     private Integer getUserIdByEmail(String email) {
         return jdbcTemplate.queryForObject(SQL_GET_USER_ID_BY_EMAIL, new Object[]{email}, Integer.class);
+    }
+
+    private void moveEmailToTrash(Integer emailId) {
+        jdbcTemplate.update(SQL_MOVE_EMAIL_TO_TRASH, emailId);
+    }
+
+    private void deleteEmailFromDB(Integer emailId) {
+        jdbcTemplate.update(SQL_DELETE_EMAIL_BY_ID, emailId);
+    }
+
+    private Boolean isEmailInTrash(Integer emailId) {
+        return jdbcTemplate.queryForObject(SQL_IS_EMAIL_IN_TRASH, new Object[]{emailId}, Boolean.class);
     }
 }
