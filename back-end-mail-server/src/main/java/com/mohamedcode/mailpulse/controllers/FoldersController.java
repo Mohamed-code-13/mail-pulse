a698e9189eea17e5deb84b3ae5568df216f1fa39
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-02-25 21:05:20 +0200

    validate the token.

--- back-end-mail-server/src/main/java/com/mohamedcode/mailpulse/controllers/FoldersController.java
+++ back-end-mail-server/src/main/java/com/mohamedcode/mailpulse/controllers/FoldersController.java
@@ -1,5 +1,9 @@
 package com.mohamedcode.mailpulse.controllers;
 
+import com.mohamedcode.mailpulse.Constants;
+import com.mohamedcode.mailpulse.exceptions.CustomAuthException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +18,7 @@ public class FoldersController {
 
     @GetMapping("/getfolders")
     public ResponseEntity<Map<String, Object>> getFolders(@RequestHeader("Authorization") String authorization) {
+        int userId = getUserId(authorization);
         Map<String, Object> map = new HashMap<>();
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
@@ -21,6 +26,7 @@ public class FoldersController {
     @PostMapping("/createfolder")
     public ResponseEntity<Map<String, Object>> createFolder(@RequestHeader("Authorization") String authorization,
                                                             @RequestParam("foldername") String folderName) {
+        int userId = getUserId(authorization);
         Map<String, Object> map = new HashMap<>();
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
@@ -28,6 +34,7 @@ public class FoldersController {
     @DeleteMapping("/deletefolder")
     public ResponseEntity<Map<String, Object>> deleteFolder(@RequestHeader("Authorization") String authorization,
                                                             @RequestParam("foldername") String folderName) {
+        int userId = getUserId(authorization);
         Map<String, Object> map = new HashMap<>();
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
@@ -35,7 +42,24 @@ public class FoldersController {
     @GetMapping("/folderemails")
     public ResponseEntity<Map<String, Object>> getFolderEmails(@RequestHeader("Authorization") String authorization,
                                                             @RequestParam("foldername") String folderName) {
+        int userId = getUserId(authorization);
         Map<String, Object> map = new HashMap<>();
         return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+    private Integer getUserId(String authorization) {
+        String[] authHeaders = authorization.split("Bearer ");
+
+        String token = authHeaders[1];
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(Constants.SECRET_API_KEY)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return Integer.parseInt(claims.get("user_id").toString());
+        } catch (Exception e) {
+            throw new CustomAuthException("invalid/expired token");
+        }
     }
 }
