bbb01f506b0b1b6e06b2e57f0b8e3533379a8748
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-02-28 23:19:41 +0200

    Creating contact service interface with the required methods

---
+++ back-end-mail-server/src/main/java/com/mohamedcode/mailpulse/services/ContactsService.java
@@ -0,0 +1,16 @@
+package com.mohamedcode.mailpulse.services;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ContactsService {
+
+    Map<String, Object> getContacts(Integer userId);
+
+    void createContact(Integer userId, String name, List<String> emails);
+
+    void deleteContact(Integer userId, List<Integer> contactIds);
+
+    void updateContact(Integer userId, Integer contactId, String name, List<String> emails);
+    
+}
